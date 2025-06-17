bb71628e1f92e951e966cdacba7d12ea6846801a
Mariam Haitham <haithammariam36@gmail.com>
2025-06-17 13:51:05 +0300

    Update Reset Password UI

---
+++ frontend/app/ResetPassword/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "../../Login/page.module.css";
+
+const Header = () => {
+    return (
+        <div className={styles.titleSubtitleContainer}>
+            <h2 className={styles.title}>Reset Password</h2>
+        </div>
+    );
+};
+
+export default Header;
