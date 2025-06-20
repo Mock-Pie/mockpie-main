9336a438fb67afdcf29f02a5c7e49fd3defa241f
Mariam Haitham <haithammariam36@gmail.com>
2025-06-20 19:45:50 +0300

    login page is done

--- frontend/app/Login/page.tsx
+++ frontend/app/Login/page.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import styles from "./page.module.css";
-import SideBar from "./components/SideBar";
-import Header from "./components/Header";
 import LoginForm from "./components/LoginForm";
-import LoginImage from "./components/LogInImage";
 
 
 const LoginPage = () => {
     return (
         <div className={styles.container}>
-        <SideBar />  
-        <Header />
         <LoginForm />
-        <LoginImage />
         </div>
     );
 };
