4983d310a558d24e8c8124faab872b3096dd2801
Mariam Haitham <haithammariam36@gmail.com>
2025-06-21 04:07:02 +0300

    Update Design for forgot password

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -31,6 +31,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://kit.fontawesome.com/a076d05399.js" crossOrigin="anonymous"></script>
+      </head>
       <body 
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable}`}
         suppressHydrationWarning={true}
