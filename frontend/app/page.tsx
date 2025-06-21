703c6fa64845de0f32b700932bd8b4226763a866
Mariam Haitham <haithammariam36@gmail.com>
2025-06-21 04:09:01 +0300

    OTP Verification

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import SignUpPage from "./SignUp/page";
 import SubmittedTrials from "./SubmittedTrials/page";
+import LoginPage from "./Login/page";
 
 const HomePage = () => {
     return (
         // <SignUpPage />
-        <SubmittedTrials />
+        // <SubmittedTrials />
+        <LoginPage />
     );
 }
 
