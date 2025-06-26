bdf4dfe7c322ab9a4fd8f6f401c7bdbb798c109d
nooreyad <nooreyadd39@gmail.com>
2025-06-26 15:39:57 +0300

    No code changes detected; skipping commit.

--- backend/app/schemas/user/user_profile_schema.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-from datetime import datetime
-from backend.app.schemas.user.user_schema import UserResponse
-from backend.app.schemas.presentation.presentation_schema import PresentationResponse
