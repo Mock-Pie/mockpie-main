98d5ef29ab19be7b1c03c8bd2197b7f6cdd57076
Mariam Haitham <96283723+mariam1h@users.noreply.github.com>
2025-06-30 13:44:42 +0300

    Update Calendar header and layout styles

--- frontend/app/Calendar/page.tsx
+++ frontend/app/Calendar/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "../UploadRecordVideos/page.module.css";
+import uploadStyles from "../UploadRecordVideos/uploadrecord.module.css";
 import SideBar from "../UploadRecordVideos/components/SideBar";
 import Header from "./components/Header";
 
@@ -7,8 +8,11 @@ import Header from "./components/Header";
 const Calendar= () => {
     return (
         <div className={styles.container}>
-        <SideBar />  
-        <Header />
+            <SideBar />  
+            <div className={uploadStyles.mainContent}>
+                <Header />
+                {/* Calendar content will go here */}
+            </div>
         </div>
     );
 };
